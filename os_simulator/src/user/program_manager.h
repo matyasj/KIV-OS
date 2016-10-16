fcd6e4b167eff16e7943016e8f935610d44d15bd
maresva <maresva@students.zcu.cz>
2016-10-16 15:43:13 +0200

    Pridano zpracovani vektoru commandu

--- os_simulator/src/user/program_manager.h
+++ os_simulator/src/user/program_manager.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "Parser\command.h"
 
-std::string execute_command(Command);
+std::string execute_commands(std::vector<Command>);
+std::string start_program(Command command, std::string input);
