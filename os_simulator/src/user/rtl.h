c4f505003af8a85b653590c6614cc5fd2857e0db
matyasj <matya.ji@seznam.cz>
2016-10-30 18:27:07 +0100

    Mala zmena nazvu promene v rtl.h

--- os_simulator/src/user/rtl.h
+++ os_simulator/src/user/rtl.h
@@ -8,7 +8,7 @@ size_t Get_Last_Error();
 THandle Create_File(const char* file_name, size_t flags);
 		//podle flags otevre, vytvori soubor a vrati jeho deskriptor
 		//vraci nenulovy handle, kdyz vse OK
-bool Write_File(const THandle file_handle, const void *buffer, const size_t buffer_size, size_t &written);
+bool Write_File(const THandle file_handle, const void *buffer, const size_t flag, size_t &written);
 		//zapise do souboru identifikovaneho deskriptor data z buffer o velikosti buffer_size a vrati pocet zapsanych dat ve writtent
 		//vraci true, kdyz vse OK
 bool Append_File(const THandle file_handle, const void *buffer, const size_t buffer_size, size_t &written);
